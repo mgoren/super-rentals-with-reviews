9b1220f170afa70c92a218483dadb4b0be1a5609
Mike Goren <mike@mortalwombat.net>
2016-10-09 15:18:29 -0700

    add ability to update rentals

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  updateRentalForm: false,
   actions: {
-    imageShow: function() {
+    imageShow() {
       this.set('isImageShowing', true);
     },
-    imageHide: function() {
+    imageHide() {
       this.set('isImageShowing', false);
+    },
+    update(rental, params) {
+      this.sendAction('update', rental, params);
     },
     delete(rental) {
       if (confirm('Are you sure you want to delete this rental?')) {
