61b60d7a4a94431a1d9aa2133e274311f0f1f7b9
Mike Goren <mike@mortalwombat.net>
2016-10-09 15:14:27 -0700

    add ability to create new rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,12 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
