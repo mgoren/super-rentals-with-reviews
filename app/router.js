bafc3b714e1e1aa127b6c63b8f35bbeedb3f5ab9
Mike Goren <mike@mortalwombat.net>
2016-10-09 15:28:15 -0700

    add rental route

--- app/router.js
+++ app/router.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+  location: config.locationType
 });
 
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('rental', {path: '/rental/:rental_id'});
 });
 
 export default Router;
