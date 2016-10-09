7fa00c74e3d238aaf423443c8bb029fa598dea1b
Mike Goren <mike@mortalwombat.net>
2016-10-09 15:41:15 -0700

    add review model

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-     return this.store.findAll('rental');
-   },
-   actions: {
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
+  },
+  actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
+      this.transitionTo('index');
+    },
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      newReview.save();
       this.transitionTo('index');
     }
   }
