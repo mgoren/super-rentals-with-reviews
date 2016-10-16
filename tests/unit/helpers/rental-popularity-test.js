53a94eced4327a0d30c8a25235dcbcb6c7f4fb1b
Mike Goren <mike@mortalwombat.net>
2016-10-16 19:59:34 -0400

    add rental popularity and cost helpers

---
+++ tests/unit/helpers/rental-popularity-test.js
@@ -0,0 +1,10 @@
+import { rentalPopularity } from 'super-rentals/helpers/rental-popularity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | rental popularity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = rentalPopularity([42]);
+  assert.ok(result);
+});
